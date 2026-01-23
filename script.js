0959278480c2e0ba3f240106cef7268c4f2b0599
Cjcrump <251788389+CjCrump@users.noreply.github.com>
2026-01-23 00:01:05 -0600

    completely changed all text and branding and set up for generic business

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const contactButtons = document.querySelectorAll(".contact-btn");
 
-const portfolioContactUrl =
-  "https://CjCrump.github.io/personal-portfolio/#contact";
-
 contactButtons.forEach(button => {
   button.addEventListener("click", () => {
-    window.location.href = portfolioContactUrl;
+    document.getElementById("contact").scrollIntoView({
+      behavior: "smooth"
+    });
   });
 });
+
